0215ae3d0a0ba1145cede1a0342f4d9e56c2382e
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-03-08 12:37:49 -0800

    Wee

--- FPS Dummy 64/Assets/Scripts/GunScripts.cs
+++ FPS Dummy 64/Assets/Scripts/GunScripts.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class GunScripts : MonoBehaviour
 {
-    public GameObject[] bullets;
-    public GameObject originPoint;
-    public float bulletSpeed = 10;
-    public Rigidbody bullet;
+    public Rigidbody projectile;
+    public Rigidbody shell;
+    public float speed = 1000;
+    public float shellSpeed = 1000;
 
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var newBullet = Instantiate(projectile, transform.position, transform.rotation);
+            var newShell = Instantiate(shell, transform.position, transform.rotation);
+            // Might have to tweak the direction using -transform.forward according to your needs
+            newBullet.velocity = transform.forward * speed;
+            newShell.velocity = transform.up * shellSpeed;
 
-    void Fire()
-    {
-        Rigidbody bulletsClone = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
-        bulletsClone.velocity = transform.forward * bulletSpeed;
+            Debug.Log("Shoot!");
+        }
     }
-    private void Update()
-    {
-        if (Input.GetButtonDown("Fire1"))
-            Fire();
-    }
-    
 
 }
