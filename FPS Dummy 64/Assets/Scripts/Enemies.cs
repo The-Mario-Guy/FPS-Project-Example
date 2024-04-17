c9da758d0d0ffc1711309276f187ca403865a0f9
The-Mario-Guy <111007100+The-Mario-Guy@users.noreply.github.com>
2024-04-17 12:06:40 -0700

    WEEE

---
+++ FPS Dummy 64/Assets/Scripts/Enemies.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Enemies : MonoBehaviour
+{
+	public TextMeshProUGUI enemyCount;
+	// Use this for initialization
+	int enemiesLeft = 0;
+	bool killedAllEnemies = false;
+	void Start()
+	{
+		enemiesLeft = 10; // or whatever;
+		enemyCount.text = enemiesLeft.ToString();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Goomba");
+		enemiesLeft = enemies.Length;
+		enemyCount.text = enemiesLeft.ToString();
+		if (enemiesLeft == 0)
+		{
+			endGame();
+		}
+	}
+
+	void endGame()
+	{
+		killedAllEnemies = true;
+	}
+
+	void OnGUI()
+	{
+		if (killedAllEnemies)
+		{
+			GUI.Label(new Rect(0, 0, 200, 20), "all gone");
+		}
+		else
+		{
+			GUI.Label(new Rect(0, 0, 200, 20), "Enemies Remaining : " + enemiesLeft);
+		}
+	}
+}
